075c71ff77e9a4d93a0631e6a0c167d10d3cbffd
engler <ddd.rrr.eee@gmail.com>
2021-04-07 21:06:29 -0700

    added libunix

---
+++ libunix/strcatf.c
@@ -0,0 +1,36 @@
+#include <stdarg.h>
+#include <string.h>
+#include "libunix.h"
+
+char *strcatf(char *dst, const char *fmt, ...) {
+    char buf[4096];
+
+    va_list args;
+    va_start(args, fmt);
+        if(vsnprintf(buf, sizeof buf, fmt, args) < 0)
+            panic("overflowed buffer\n");
+    va_end(args);
+    return strcat(dst, buf);
+}
+
+char *strcpyf(char *dst, const char *fmt, ...) {
+    char buf[4096];
+
+    va_list args;
+    va_start(args, fmt);
+        if(vsnprintf(buf, sizeof buf, fmt, args) < 0)
+            panic("overflowed buffer\n");
+    va_end(args);
+    return strcpy(dst, buf);
+}
+
+char *strdupf(const char *fmt, ...) {
+    char buf[4096];
+
+    va_list args;
+    va_start(args, fmt);
+        if(vsnprintf(buf, sizeof buf, fmt, args) < 0)
+            panic("overflowed buffer\n");
+    va_end(args);
+    return strdup(buf);
+}
