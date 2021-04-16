8b01854552f9eb5488ee7d6e5a225c0e19d5def7
engler <ddd.rrr.eee@gmail.com>
2021-04-16 15:47:54 -0700

    minor

---
+++ labs/4-uart/bootloader-pi-side/put-your-src-here.mk
@@ -0,0 +1,12 @@
+# if you want to replace our .o with yours do as follows:
+#   1. add yours to YOUR_SRC
+#   2. remove the corresponding from from STAFF_OBJS
+
+# put the path to any .S files.
+YOUR_ASM = 
+
+# put the path to any .c files
+YOUR_SRC = src/hello.c src/gpio.c src/uart.c
+
+# these are initial .o's we give you: to use your own, remove them
+STAFF_OBJS= staff-objs/gpio-pud.o 
